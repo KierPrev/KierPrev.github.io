d9085339a99ada70c024ed073118f9906ad9fc38
Kiernan E. Preve <44746791+KierPrev@users.noreply.github.com>
2023-12-16 02:42:01 -0300

    calculadora inflación y gastos

---
+++ calcu/js/calcular.js
@@ -0,0 +1,13 @@
+function gastoMes() {
+    gastoHoy = Number(document.getElementById("gastos").value);
+    gastoTotal = gastoHoy;
+    plataTotal = Number(document.getElementById("billetera").value);
+
+    for (let i = 1; i < 30; i++) {
+      gastoTotal = gastoTotal + gastoHoy*(1+0.01*i);
+    }
+
+    document.getElementById("gastoTotal").innerHTML = String(gastoTotal.toLocaleString('en-US'));
+
+    document.getElementById("plataTotal").innerHTML = String((plataTotal - gastoTotal).toLocaleString('en-US'));
+  }
